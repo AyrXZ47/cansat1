8851982995ace88cd84194e235512572d3a69a7b
Ndahai Arenas <gumojammbo@gmail.com>
2024-07-04 11:21:42 -0600

    Mover valores de graficas a la clase constantes

--- Cansat/gui/ui/graphs/temperature_graph.py
+++ Cansat/gui/ui/graphs/temperature_graph.py
@@ -1,24 +1,25 @@
 import pyqtgraph as qtgraph
 import numpy
 from PyQt6.QtCore import QTimer, Qt
+from Cansat.gui.utils.constants import *
 
-# TODO Necesita refactor, solo es para probar :p
 class TemperatureGraph(qtgraph.PlotWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        pen_color = (255, 0, 0)
-        pen_width = 3
+        pen_color = (TEMP_COLOR)
+        pen_width = GRAPH_PENWIDTH
         self.temp_plot = self.plot(pen=qtgraph.mkPen(color=pen_color, width=pen_width, antialias=True, style=Qt.PenStyle.DashLine))
         self.temp_data = numpy.linspace(0, 0, 30)
         self.ptr = 0
-        self.setBackground('w')
-        self.setMouseEnabled(x=False, y=False)
+        self.setBackground(GRAPH_BACKGROUND)
+        self.setMouseEnabled(x=GRAPH_ENABLEMOUSE, y=GRAPH_ENABLEMOUSE)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_data)
-        self.timer.start(500)
+        self.timer.start(GRAPH_UPDINTERVAL)
 
 
+    # TODO Actualizar método para recibir datos de la estacion terrena
     def update_data(self):
         new_data = numpy.random.normal(size=30)
         self.temp_data = numpy.roll(self.temp_data, -1)
@@ -26,7 +27,3 @@ class TemperatureGraph(qtgraph.PlotWidget):
         self.temp_plot.setData(self.temp_data)
 
 
-
-    # def init_graph(self):
-    #     x = numpy.linspace(0, 100, 50)
-    #     self.plo
